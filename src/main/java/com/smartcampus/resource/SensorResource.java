61a99fd7b9f3e16a781655f145259ead3842bc56
ImSam_dev <imashansam@gmail.com>
2026-04-17 20:35:41 +0530

    Add sensor filtering with query parameter

--- src/main/java/com/smartcampus/resource/SensorResource.java
+++ src/main/java/com/smartcampus/resource/SensorResource.java
@@ -1,4 +1,53 @@
 package com.smartcampus.resource;
 
+import com.smartcampus.model.Sensor;
+import com.smartcampus.storage.DataStore;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.QueryParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/sensors")
 public class SensorResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Sensor> getSensors() {
+
+        return new ArrayList<>(DataStore.sensors.values());
+
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createSensor(Sensor sensor) {
+
+        DataStore.sensors.put(sensor.getId(), sensor);
+
+        return Response.status(Response.Status.CREATED).entity(sensor).build();
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Sensor> getSensors(@QueryParam("type") String type) {
+
+        List<Sensor> result = new ArrayList<>();
+
+        for (Sensor sensor : DataStore.sensors.values()) {
+
+            if (type == null || sensor.getType().equalsIgnoreCase(type)) {
+                result.add(sensor);
+            }
+
+        }
+
+        return result;
+    }
 }
