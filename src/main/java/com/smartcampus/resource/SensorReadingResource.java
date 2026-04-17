ac2f294ebcbacf15cefb9ad2bd19a5c7f3e20be7
ImSam_dev <imashansam@gmail.com>
2026-04-17 21:15:58 +0530

    Add sensor readings endpoint

--- src/main/java/com/smartcampus/resource/SensorReadingResource.java
+++ src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -1,4 +1,32 @@
 package com.smartcampus.resource;
 
+import com.smartcampus.model.SensorReading;
+import com.smartcampus.storage.DataStore;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/sensors/{id}/readings")
 public class SensorReadingResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addReading(@PathParam("id") String sensorId, SensorReading reading) {
+
+        List<SensorReading> sensorReadings =
+                DataStore.readings.getOrDefault(sensorId, new ArrayList<>());
+
+        sensorReadings.add(reading);
+
+        DataStore.readings.put(sensorId, sensorReadings);
+
+        return Response.status(Response.Status.CREATED).entity(reading).build();
+    }
 }
--- src/main/java/com/smartcampus/storage/DataStore.java
+++ src/main/java/com/smartcampus/storage/DataStore.java
@@ -5,10 +5,12 @@ import com.smartcampus.model.Sensor;
 
 import java.util.HashMap;
 import java.util.Map;
+import com.smartcampus.model.SensorReading;
+import java.util.List;
 
 public class DataStore {
 
     public static Map<String, Room> rooms = new HashMap<>();
     public static Map<String, Sensor> sensors = new HashMap<>();
-
+    public static Map<String, List<SensorReading>> readings = new HashMap<>();
 }
