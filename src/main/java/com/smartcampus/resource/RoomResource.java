833e03dc5bf923f41953b412c7e4faf012e57c28
ImSam_dev <imashansam@gmail.com>
2026-04-17 19:22:54 +0530

    Add Room CRUD endpoints (GET all, POST, GET by id)

--- src/main/java/com/smartcampus/resource/RoomResource.java
+++ src/main/java/com/smartcampus/resource/RoomResource.java
@@ -6,7 +6,12 @@ import com.smartcampus.storage.DataStore;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,5 +25,27 @@ public class RoomResource {
 
         return new ArrayList<>(DataStore.rooms.values());
 
+    }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createRoom(Room room) {
+
+        DataStore.rooms.put(room.getId(), room);
+
+        return Response.status(Response.Status.CREATED).entity(room).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRoomById(@PathParam("id") String id) {
+
+        Room room = DataStore.rooms.get(id);
+
+        if (room == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(room).build();
     }
 }
